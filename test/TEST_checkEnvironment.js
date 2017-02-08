5118f4252eb1ac7759caff250d127b33947be27a
Ian Laird <irlaird@gmail.com>
2017-02-08 12:04:06 -0700

    Added unit testing framework.

---
+++ test/TEST_checkEnvironment.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Environment', function() {
+  describe('General Environment Test', function() {
+    it('Should succeed', function() {
+      assert(true);
+    });
+  });
+});
